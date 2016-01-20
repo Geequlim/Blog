eb97b350b82bd527a746beaa60cc2728c9c5ed4f
Geequlim <geequlim@gmail.com>
2016-01-21 03:29:36 +0800

    Reset placeholde

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -9,7 +9,9 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        本站正在维护中，请稍后访问...
+        This site is under developping...
+        <br/>
+        You may visit here later :)
       </div>
     );
   }
