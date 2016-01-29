c631104e3a88dbfc8c52c036c37491359b9173ec
Geequlim <geequlim@gmail.com>
2016-01-29 22:30:20 +0800

    Add footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import site from 'json!yaml!../../data/site.yaml';
-import IconLink from './IconLink.jsx';
 
 class Footer extends React.Component {
   constructor(props) {
@@ -8,42 +7,60 @@ class Footer extends React.Component {
     this.state = {};
   }
 
+  asMobilePhone() {
+    return this.state.windowWidth < 768;
+  }
+
   render() {
     return (
-      <div className="footer">
-        <div className="ui attached stackable menu">
-          <div className="ui container">
-            <a className="item">
-              <i className="home icon"></i>
-              Home
-            </a>
-            <a className="item">
-              <i className="grid layout icon"></i>
-              Browse
-            </a>
-            <a className="item">
-              <i className="mail icon"></i>
-              Messages
-            </a>
-            <div className="ui simple dropdown item">
-              More
-              <i className="dropdown icon"></i>
-              <div className="menu">
-                <a className="item">
-                  <i className="edit icon"></i>
-                  Edit Profile</a>
-                <a className="item">
-                  <i className="globe icon"></i>
-                  Choose Language</a>
-                <a className="item">
-                  <i className="settings icon"></i>
-                  Account Settings</a>
-              </div>
-            </div>
-            <div className="right item">
-              <div className="ui input"><input type="text" placeholder="Search..."/></div>
-            </div>
-          </div>
+      <div className={`footer ${window.innerWidth < 768 ? '' : 'row'}`}>
+        <div className="info">
+          <p className="ui medium header">{site.title}</p>
+          <p className="ui small header">{site.subtitle}</p>
+        </div>
+        <div className="info">
+          <p>Find {site.author.name} in social network</p>
+          { site.author.email ?
+            (
+              <p>
+                <i className="mail icon"/>
+                <a href={`mailto:${site.author.email}`}>{site.author.email}</a>
+              </p>
+            ) : null
+          }
+          { site.author.github_username ?
+            (
+              <p className="ui label">
+                <i className="github icon"/>
+                <a href={`https://github.com/${site.author.github_username}`}>
+                  {site.author.github_username}
+                </a>
+              </p>
+            ) : null
+          }
+          { site.author.twitter_username ?
+            (
+              <p className="ui blue label">
+                <i className="twitter icon"/>
+                <a href={`https://twitter.com/${site.author.twitter_username}`}>
+                  {site.author.github_username}
+                </a>
+              </p>
+            ) : null
+          }
+          { site.author.weibo_userpage && site.author.weibo_username ?
+            (
+              <p className="ui red label">
+                <i className="weibo icon"/>
+                <a href={site.author.weibo_userpage}>
+                  {site.author.weibo_username}
+                </a>
+              </p>
+            ) : null
+          }
+        </div>
+        <div className="column">
+          <p>{site.description}</p>
         </div>
       </div>
     );
