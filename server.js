6363c5b255a3206c93835d44e1885bc9d379be02
Geequlim <geequlim@gmail.com>
2016-02-02 11:01:10 +0800

    Restore server.js

--- server.js
+++ server.js
@@ -11,14 +11,5 @@ app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '', 'index.html'));
 });
 
-app.all('*', function(req, res, next) {
-  res.set({
-    'Access-Control-Allow-origin': '*',
-    'Access-Control-Allow-Headers': 'X-Requested-With',
-    'Access-Control-Allow-Methods': 'GET'
-  });
-  next();
-});
-
 app.listen(port);
 console.log('server started on port ' + port);
