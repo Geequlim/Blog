a4ef9abfc3faafd8893cc9c211e61c2a721d4c9f
Geequlim <geequlim@gmail.com>
2016-08-15 22:32:06 +0800

    Disqus添加加载进度标志

--- app/src/components/Disqus.jsx
+++ app/src/components/Disqus.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import $ from 'jquery';
 import app from '../app';
+import LoadingFlag from "./LoadingFlag.jsx"
 class Disqus extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        };
+    }
 
-  componentDidMount() {
-    const url = `http://${app.site.disqusShortName}.disqus.com/embed.js`;
-    $.ajax({
-      type: 'GET',
-      url,
-      dataType: 'script',
-      cache: true
-    });
-  }
-  render() {
-    return (
-      <div id="disqus_thread"></div>
-    );
-  }
+    componentDidMount() {
+        const self = this;
+        const url = `http://${app.site.disqusShortName}.disqus.com/embed.js`;
+        $.ajax({type: 'GET', url, dataType: 'script', cache: true}).done(() => self.setState({loading: false}));
+    }
+    render() {
+        return (
+            <div id="disqus_thread">
+                {this.state.loading?<LoadingFlag>{app.string.disqus_loading}</LoadingFlag>:null}
+            </div>
+        );
+    }
 }
 
 module.exports = Disqus;
