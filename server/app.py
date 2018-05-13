fb75158ec2114ff3260a50ac90667d83bdb29e87
geequlim <geequlim@gmail.com>
2018-05-13 14:06:24 +0800

    cleanup the server start up code

--- server/app.py
+++ server/app.py
@@ -6,40 +6,42 @@ from src.api import api, config, database
 from gevent.pywsgi import WSGIServer
 
 def main():
-	debug = False
-	host  = '0.0.0.0'
+	debug = True if os.getenv("SERVER_DEBUG") != None else False
+	host = '0.0.0.0'if not debug else '127.0.0.1'
+	port = int(os.getenv("SERVER_PORT")) if os.getenv("SERVER_PORT") != None else 5000
 	ssl_key = 'cert/server.key'
 	ssl_cert = 'cert/server.crt'
-	if os.getenv("SERVER_DEBUG") != None:
-		debug = True
-		host = '127.0.0.1'
-		print(config)
-		api.app.run(host=host, debug=debug, ssl_context=(ssl_cert, ssl_key))
+	protocal = 'http' if debug else 'https'
+
+	print('Starting debug server at {0}://{1}:{2}'.format(protocal, host, port))
+	print(config)
+
+	if debug:
+		api.app.run(host=host, port=port, debug=debug)
 	else:
-		WSGIServer((host, 5000), api.app, keyfile=ssl_key, certfile=ssl_cert).serve_forever()
-
-def import_posts(root, author):
-	import yaml
-	from dateutil import parser as dateparser
-	from datetime import datetime, date
-	from src.database import Post
-	from utils import uid32
-	import json
-	for rp in yaml.load(open(root + "/assets/posts.yaml")):
-		print("import post:", rp['title'])
-		p = Post.create()
-		p.object_id = uid32(rp['id'])
-		p.created_at = dateparser.parse(str(rp['publishAt']))
-		p.title = rp['title']
-		p.content = open(root + rp['file']).read()
-		tags = list(set(rp['categories'] + rp['tags']))
-		if 'C++' in tags and 'C/C++' in tags:
-			tags.remove('C++')
-		p.tags = json.dumps(tags, ensure_ascii=False)
-		p.save()
-
+		WSGIServer((host, port), api.app, keyfile=ssl_key, certfile=ssl_cert).serve_forever()
 
 if __name__ == '__main__':
 	# import_posts("/home/geequlim/Documents/Workspace/Develop/Web/Blog/v1", "3641a55bf8e94f9fb5238d26f1911a52")
 	# api.db.database.create_table(database.Fragment)
 	main()
+
+# def import_posts(root, author):
+# 	import yaml
+# 	from dateutil import parser as dateparser
+# 	from datetime import datetime, date
+# 	from src.database import Post
+# 	from utils import uid32
+# 	import json
+# 	for rp in yaml.load(open(root + "/assets/posts.yaml")):
+# 		print("import post:", rp['title'])
+# 		p = Post.create()
+# 		p.object_id = uid32(rp['id'])
+# 		p.created_at = dateparser.parse(str(rp['publishAt']))
+# 		p.title = rp['title']
+# 		p.content = open(root + rp['file']).read()
+# 		tags = list(set(rp['categories'] + rp['tags']))
+# 		if 'C++' in tags and 'C/C++' in tags:
+# 			tags.remove('C++')
+# 		p.tags = json.dumps(tags, ensure_ascii=False)
+# 		p.save()
--- server/debug.sh
+++ server/debug.sh
@@ -2,4 +2,5 @@
 cd $(cd "$(dirname "$0")"; pwd)
 source ENV/bin/activate
 export SERVER_DEBUG="1"
+export SERVER_PORT="5000"
 python3 app.py
