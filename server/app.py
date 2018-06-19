5bd25fe618ff83933f5ee35a080d7c6433eeb5a4
geequlim <geequlim@gmail.com>
2018-06-19 13:41:01 +0800

    Add http protocal start script

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-#! /usr/sbin/env python3
+#! /usr/bin/env python3
 import os
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
@@ -9,9 +9,10 @@ def main():
 	debug = True if os.getenv("SERVER_DEBUG") != None else False
 	host = '0.0.0.0'if not debug else '127.0.0.1'
 	port = int(os.getenv("SERVER_PORT")) if os.getenv("SERVER_PORT") != None else 5000
-	ssl_key = 'cert/server.key'
-	ssl_cert = 'cert/server.crt'
+	ssl_key = os.getenv("SSL_KEY") if os.getenv("SSL_KEY") != None else None
+	ssl_cert = os.getenv("SSL_CERT") if os.getenv("SSL_CERT") != None else None
 	protocal = 'http' if debug else 'https'
+	protocal = 'http' if None == ssl_key or ssl_cert == None else protocal
 
 	print('Starting debug server at {0}://{1}:{2}'.format(protocal, host, port))
 	print(config)
@@ -19,11 +20,10 @@ def main():
 	if debug:
 		api.app.run(host=host, port=port, debug=debug)
 	else:
-		http_server = HTTPServer(WSGIContainer(api.app), ssl_options={"certfile": ssl_cert, "keyfile": ssl_key})
+		ssl_options= {"certfile": ssl_cert, "keyfile": ssl_key} if protocal == 'https' else None
+		http_server = HTTPServer(WSGIContainer(api.app), ssl_options=ssl_options)
 		http_server.listen(address=host, port=port)
 		IOLoop.instance().start()
 
 if __name__ == '__main__':
-	# api.db.database.create_table(database.Fragment)
-	# api.db.database.
 	main()
--- server/start.http.sh
+++ server/start.http.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
 cd $(cd "$(dirname "$0")"; pwd)
 source ENV/bin/activate
-export SSL_CERT="cert/server.crt"
-export SSL_KEY="cert/server.key"
 python3 app.py &
